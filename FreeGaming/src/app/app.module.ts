509fc8d1a2fdbf471d5c841eed3bc2b6deef7c48
Icare741 <94448198+Icare741@users.noreply.github.com>
2023-03-23 15:26:03 +0100

    init :tada:

--- FreeGaming/src/app/app.module.ts
+++ FreeGaming/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { NavComponent } from './core/components/nav/nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+
   ],
   imports: [
     BrowserModule,
